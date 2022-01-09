576ecd0c9600b3d896257ecbf828b0c1f1a1f344
npee <jmc4664@gmail.com>
2022-01-09 17:06:44 +0900

    for ~ in

--- bin/3_control_flow/f_for_loop.dart
+++ bin/3_control_flow/f_for_loop.dart
@@ -1,9 +1,17 @@
 void main() {
   forLoop();
+  forLoopWithoutIndex();
 }
 
 void forLoop() {
   for (var i = 0; i < 5; i++) {
     print(i);
+  }
+}
+
+void forLoopWithoutIndex() {
+  List<String> pets = ['Odyn', 'Buck', 'Yeti'];
+  for (var pet in pets) {
+    print(pet);
   }
 }
