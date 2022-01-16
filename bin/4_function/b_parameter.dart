bf915ab7ae026cc8e8461ff7b462f5ea4be1b413
npee <jmc4664@gmail.com>
2022-01-16 23:19:48 +0900

    이름 지정 파라미터

--- bin/4_function/b_parameter.dart
+++ bin/4_function/b_parameter.dart
@@ -1,7 +1,12 @@
 void main() {
   debugger('A bug!', 55);
+  debuggerNamedParameter(lineNum: 44);
 }
 
 void debugger(String message, int lineNum) {
+  print('$message (line: $lineNum)');
+}
+
+void debuggerNamedParameter({String message = 'A bug!', int? lineNum}) {
   print('$message (line: $lineNum)');
 }
