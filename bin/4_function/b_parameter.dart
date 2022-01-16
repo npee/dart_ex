a6715186fdc26377db2f86f8d6e85f39a33f1b6e
npee <jmc4664@gmail.com>
2022-01-17 08:30:59 +0900

    선택적 파라미터

--- bin/4_function/b_parameter.dart
+++ bin/4_function/b_parameter.dart
@@ -1,6 +1,8 @@
 void main() {
   debugger('A bug!', 55);
   debuggerNamedParameter(lineNum: 44);
+  debuggerOptionalParameter('A bug!', 66, '[', ']');
+  debuggerOptionalParameter('A bug!', 66);
 }
 
 void debugger(String message, int lineNum) {
@@ -9,4 +11,8 @@ void debugger(String message, int lineNum) {
 
 void debuggerNamedParameter({String message = 'A bug!', int? lineNum}) {
   print('$message (line: $lineNum)');
+}
+
+void debuggerOptionalParameter(String message, int lineNum, [String openTag = '(', String closeTag = ')']) {
+  print('$message $openTag''line: $lineNum$closeTag');
 }
