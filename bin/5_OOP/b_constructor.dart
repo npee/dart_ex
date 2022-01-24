1acc1f09f9e6e82ac8dbb4b5c0e3b70c7dc7666a
npee <jmc4664@gmail.com>
2022-01-24 12:30:51 +0900

    생성자

---
+++ bin/5_OOP/b_constructor.dart
@@ -0,0 +1,18 @@
+void main() {
+
+}
+
+class Animal {
+  String? name;
+  String? type;
+
+  Animal(this.name, this.type) {
+    print('Hello from Animal!');
+  }
+
+  // Animal(String name, String type) {
+  //   this.name = name;
+  //   this.type = type;
+  //   print('Hello from Animal!');
+  // }
+}
